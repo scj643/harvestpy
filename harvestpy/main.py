5fdece63f567d0983ea707c90fd58beaed28c1b2
scj643 <surettcharles@gmail.com>
2017-01-25 17:02:52 -0500

    started work on game logic

--- harvestpy/main.py
+++ harvestpy/main.py
@@ -1,4 +1,12 @@
-
+from . player import Player
 
 class Game(object):
-    def __init__(self, 
+    def __init__(self, players = [Player()]):
+        self.players = players
+        self.day = 1
+    
+    def sleep(self):
+        for i in self.players:
+            i._sleep()
+        self.day += 1
+        
--- harvestpy/player.py
+++ harvestpy/player.py
@@ -4,5 +4,21 @@ class Player(object):
         self.gold = gold
         self.name = name
         self.aval_moves = self.max_moves
+        
+    def _do_move(self, power=1):
+        '''Do a move
+        checks if a move with a set power can be done
+        Returns true if done false if otherwise
+        '''
+        if self.aval_moves > power:
+            self.aval_moves -= power
+            return True
+        else:
+            return False
+    
+    def _sleep(self):
+        self.aval_moves = self.max_moves
+        return 'Player: [{}] slept'.format(self.name)
+        
     def __repr__(self):
         return '<Player name: {name}, aval_moves: {aval}, max_moves: {max_moves}, gold: {gold}>'.format(name = self.name, max_moves = self.max_moves, gold = self.gold, aval=self.aval_moves)
